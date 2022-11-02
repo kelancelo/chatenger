cc3e4292d69715b635d6c1b1019e87ecada1372f
Miguel Angelo L. Delos Reyes <33194805+kelancelo@users.noreply.github.com>
2022-11-02 14:50:34 +0800

    fixed issue where message is immediately read when sent

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -10,7 +10,17 @@ import '../styles/index.css'
 
 
 const socket = import.meta.env.PROD
+    // "autoConnect: false" won't automatically connect the client to the server socket
+    // so the client will be able to prepare the data to be sent to the server socket
+    // before connecting
+
+    // in PROD mode, I serve the frontend in the same domain as my server.
+    // if the frontend is served on the same domain as the server, 
+    // the URL of the server can be omitted because
+    // the URL can be deduced from the "window.location object".  
     ? io({ autoConnect: false })
+
+    // if in DEV mode, specify the URL of the local dev server 
     : io('http://localhost:5000', { autoConnect: false })
 
 // console.log(socket)
@@ -19,18 +29,17 @@ export default function Index() {
     const navigate = useNavigate()
     const { isAuthenticated, isLoading, user, logout } = useAuth0()
     const [users, setUsers] = useState([])
-    const [selectedUser, setSelectedUser] = useState('')
+    const [selectedUser, setSelectedUser] = useState('') // stores the id
     const [messages, setMessages] = useState([])
-    const [messagesCount, setMessagesCount] = useState(0)
     const [messagesToShow, setMessagesToShow] = useState([])
     const messagesRef = useRef(null)
     const chatInputRef = useRef(null)
-    // console.log('isLoading', isLoading)
-    // console.log('isAuthenticated', isAuthenticated)
+
 
     useEffect(() => {
         if (!isLoading && !isAuthenticated) navigate('/login')
         if (!isLoading && isAuthenticated) {
+            // data sent when connecting to the server
             socket.auth = {
                 user: user.given_name ?
                     {
@@ -48,38 +57,53 @@ export default function Index() {
                         picture: user.picture
                     }
             }
+
+            // connect to the server
             socket.connect()
+
+            // list of users sent by the server
             socket.on('users', (users) => setUsers(users))
+
+            // list of IDs of online users sent by the server
             socket.on('online users', (ids) => setUsers((users) => users.map((user) => {
                 if (ids.includes(user.id)) return { ...user, isOnline: true }
                 else return user
             })))
+
+            // list of messages sent and received by the user sent by the server
             socket.on('messages', (retrievedMessages) => {
                 setMessages(retrievedMessages)
-                setMessagesCount(retrievedMessages.length)
             })
+
+            // id of the user who just went online
             socket.on('user connected', (id) => setUsers((users) => users.map((user) => {
                 if (user.id === id) return { ...user, isOnline: true }
                 else return user
             })))
+
+            // show the new user the other users' contact list
             socket.on('new user', (newUser) => {
                 setUsers((users) => [...users, { ...newUser, isOnline: true }])
             })
+
+            // update the "messages" state when this event happens
             socket.on('chat message', (message) => {
                 setMessages((messages) => [...messages, message])
-                // alert(message.content)
             })
+
             socket.on('message has been read', (messageId) => setMessages((messages) => {
                 return messages.map((message) => {
                     if (message.id === messageId) return { ...message, hasBeenRead: true }
                     else return message
                 })
             }))
+
             socket.on('user disconnected', (id) => setUsers((users) => users.map((user) => {
                 if (user.id === id) return { ...user, isOnline: false }
                 else return user
             })))
         }
+
         return () => {
             socket.off('connect')
             socket.off('users')
@@ -95,31 +119,42 @@ export default function Index() {
         }
     }, [isLoading, isAuthenticated])
 
+    
+    // trigerred when messages are received from the server for the first time or 
+    // when a "chat message" event happens or
+    // when a "message has been read" even happens.
+    // it updates the messages shown in the messages window
     useEffect(() => {
-        if (messagesCount !== messages.length) {
-            setMessagesToShow(messages.filter((message) => {
-                return selectedUser === message.senderId || selectedUser === message.receiverId
-            }))
-            setMessagesCount(messages.length)
-        }
+        setMessagesToShow(messages.filter((message) => {
+            return selectedUser === message.senderId || selectedUser === message.receiverId
+        }))
     }, [messages])
 
+
+    // change the messages shown on the messages window if the selected user in the contacts list
+    // window changed
     useEffect(() => {
         setMessagesToShow(messages.filter((message) => {
             return selectedUser === message.senderId || selectedUser === message.receiverId
         }))
     }, [selectedUser])
 
+
     useEffect(() => {
         if (messagesRef.current) {
+            // scroll to the bottom of the messages list
             messagesRef.current.scrollTop = messagesRef.current.scrollHeight
 
             const observer = new IntersectionObserver((entries) => {
                 for (const entry of entries) {
+                    // if a message is visible in the messages list,
+                    // check if the message has not been read yet and
+                    // if the one reading it is the receiver so its
+                    // "hasBeenRead" property can be updated
                     if (entry.isIntersecting) {
-                        // console.log(entry.target.dataset.messageId)
                         const messageId = entry.target.dataset.messageId
-                        if (!messages.find((message) => message.id === messageId).hasBeenRead) {
+                        const message = messages.find((message) => message.id === messageId)
+                        if (!(message.hasBeenRead) && (user.sub === message.receiverId)) {
                             socket.emit('message has been read', messageId)
                         }
                         observer.unobserve(entry.target)
@@ -129,9 +164,11 @@ export default function Index() {
                 root: messagesRef.current,
                 threshold: .6
             })
+
             for (const message of messagesRef.current.children) {
                 observer.observe(message)
             }
+
             return () => {
                 for (const message of messagesRef.current.children) {
                     observer.unobserve(message)
@@ -156,13 +193,14 @@ export default function Index() {
         else alert('select a recipient first!')
     }
 
+
     if (!isLoading && isAuthenticated) return (
-        <main id="index-main">
-            <div id="greeting">
+        <div id="index-body">
+            <header>
                 <p>Hi {user.given_name ? user.given_name : user.name}!</p>
                 <a id="logout-link" onClick={() => logout({ returnTo: location.origin })}>Logout</a>
-            </div>
-            <div id="chat-container">
+            </header>
+            <main>
                 <div id="contacts-container">
                     <span id="contacts-container-title">Contacts</span>
                     <div id="contacts">
@@ -193,7 +231,7 @@ export default function Index() {
                         <button id="send-btn">Send</button>
                     </form>
                 </div>
-            </div>
-        </main>
+            </main>
+        </div>
     )
 }
--- index.js
+++ index.js
@@ -17,8 +17,72 @@ const path = require('path')
 
 app.use(express.static('frontend/dist'))
 
+
 app.all('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
+
+
+io.on('connection', async (socket) => {
+    // extract user data when a user connects to socket server
+    const { id, email, givenName, familyName, picture } = socket.handshake.auth.user
+
+    // this function saves the user in the DB if it doesn't exist 
+    const newUser = await saveUser(id, email, givenName, familyName, picture)
+    
+    // if user is new to the system, broadcast it so other users will be able to see the new user 
+    // in their contacts list.
+    if (newUser) socket.broadcast.emit('new user', newUser) 
+
+    // join user to a room identified by their id
+    socket.join(id)
+    console.log('a user has connected:', id)
+    
+    // send all users except the logged in user to the user
+    socket.emit('users', await getUsers(id))
+
+    // get online users' IDs
+    const onlineUsers = []
+    for (const [id, socket] of io.of("/").sockets) {
+        onlineUsers.push(socket.handshake.auth.user.id)
+    }
+
+    // send online users' IDs to the user
+    socket.emit('online users', onlineUsers)
+
+    // send all messages sent and received by the logged in user
+    socket.emit('messages', await getMessages(id))
+    
+    // same functionality as the "online users" event except this is for when an offline user goes online, 
+    // the online users will be able to see the status indicator change for that user.
+    socket.broadcast.emit('user connected', id)
+
+    socket.on('get messages', async (senderId, receiverId) => {
+        socket.emit('get messages', await getMessages(senderId, receiverId))
+    })
+
+    // save the message to the DB received from sender
+    socket.on('chat message', async ({ id, content, senderId, receiverId }) => {
+        const newMessage = await saveMessage(id, content, senderId, receiverId)
+        if (newMessage)
+            // send the message to sender and receiver
+            io.to(senderId).to(receiverId).emit('chat message', newMessage)
+    })
+
+    // update the message's "hasBeenRead" property when a user reads a message
+    socket.on('message has been read', async (messageId) => {
+        const { id } = await updateMessage(messageId)
+        if (id)
+            // tell the user that the update is done
+            socket.emit('message has been read', id)
+    })
+
+
+    socket.on('disconnect', () => {
+        console.log('a user has disconnected.')
+        // changes the color of status indicator for the user to red.
+        socket.broadcast.emit('user disconnected', id)
+    })
 })
 
 
@@ -43,6 +107,7 @@ async function saveUser(id, email, givenName, familyName, picture) {
     }
 }
 
+
 async function getUsers(id) {
     return await prisma.user.findMany({
         where: {
@@ -53,6 +118,7 @@ async function getUsers(id) {
     })
 }
 
+
 async function getMessages(id) {
     return await prisma.message.findMany({
         where: {
@@ -60,9 +126,13 @@ async function getMessages(id) {
                 { senderId: id },
                 { receiverId: id }
             ]
+        },
+        orderBy: {
+            date_sent: 'asc',
         }
     })
 }
+
 
 async function saveMessage(id, content, senderId, receiverId) {
     return await prisma.message.create({
@@ -75,45 +145,13 @@ async function saveMessage(id, content, senderId, receiverId) {
     })
 }
 
+
 async function updateMessage(id) {
     return await prisma.message.update({
         where: { id: id },
         data: { hasBeenRead: true }
     })
 }
-
-io.on('connection', async (socket) => {
-    const { id, email, givenName, familyName, picture } = socket.handshake.auth.user // extract user data
-    const newUser = await saveUser(id, email, givenName, familyName, picture)
-    const onlineUsers = []
-    for (const [id, socket] of io.of("/").sockets) {     // get current online users
-        onlineUsers.push(socket.handshake.auth.user.id)
-    }
-    socket.join(id)
-    console.log('a user has connected:', id)
-
-    if (newUser) socket.broadcast.emit('new user', newUser) // if user is new to the system, broadcast it so the logged in users will be able to add the new user in their contacts panel.
-    socket.emit('users', await getUsers(id))
-    socket.emit('online users', onlineUsers)
-    socket.emit('messages', await getMessages(id))
-    socket.broadcast.emit('user connected', id) // same functionality as the "online users" event except this is for when a user goes online, the current online users will be able to see the status indicator change for that user.
-
-    socket.on('get messages', async (senderId, receiverId) => {
-        socket.emit('get messages', await getMessages(senderId, receiverId))
-    })
-    socket.on('chat message', async ({ id, content, senderId, receiverId }) => {
-        const newMessage = await saveMessage(id, content, senderId, receiverId)
-        io.to(senderId).to(receiverId).emit('chat message', newMessage)
-    })
-    socket.on('message has been read', async (messageId) => {
-        const { id } = await updateMessage(messageId)
-        socket.emit('message has been read', id)
-    })
-    socket.on('disconnect', () => {
-        console.log('a user has disconnected.')
-        socket.broadcast.emit('user disconnected', id) // changes the color of status indicator for the user to red.
-    })
-})
 
 
 server.listen(port, () => console.log(`Server listening on port ${port}`))
